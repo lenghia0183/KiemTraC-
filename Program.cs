a082a4c1041cf71a150d1b88d5ee9f05c7730f8e
Lenghia <lenghia0183@gmail.com>
2023-10-20 21:23:12 +0700

    Bai kiem tra c#

---
+++ Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace KiemTra3{
+
+    class Program {
+
+        static List<SinhVienA> StudentList = new List<SinhVienA>();
+        static void Title(){
+            Console.WriteLine($"{"ID",10} {"Ten",15} {"DiaChi",10} {"Toan",10} {"Ly",10} {"Hoa",10} {"DiemUT",10} {"Tong",10}");
+        }
+
+        static void ShowList(){
+            Console.WriteLine("\t\t=====Danh sach=======");
+            Title();
+            foreach (var item in StudentList)
+            {
+                item.Xuat();
+            }
+        }
+
+
+        static bool IsEmpty(){
+            if(StudentList.Count==0){
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        static void AddStudent(){
+            SinhVienA x = new SinhVienA();
+            x.Nhap();
+            StudentList.Add(x);
+        }
+
+        static void SearchByKey(string key,string DiaChi, double TongDiem){
+            Title();
+            if(key =="DiaChi"){
+                foreach(var item in StudentList){
+                    if(item.DiaChi == DiaChi){
+                        item.Xuat();
+                    }
+                }
+            } else if(key == "TongDiem"){
+                foreach(var item in StudentList){
+                    if(item.Tong == TongDiem){
+                        item.Xuat();
+                    }
+                }
+            } else {
+                Console.WriteLine("Key không hợp lệ");
+            }
+        }
+
+        static void SortByMark(){
+            StudentList.Sort((x,y)=> x.Tong.CompareTo(y.Tong));
+            ShowList();
+        }
+
+        static void RemoveStuddent(string DiaChi){
+            bool isRemove = false;
+           for(int i =0 ;i<StudentList.Count; i++){
+            if(StudentList[i].DiaChi == DiaChi){
+                StudentList.RemoveAt(i);
+                isRemove = true;
+                i--;
+            }
+           }
+            if(isRemove){
+                if(IsEmpty()){
+                    Console.WriteLine("Danh sách trống");
+                } else {
+                    ShowList();
+                }
+                
+            } else {
+                Console.WriteLine("Địa chỉ bạn nhập vào không có trong danh sách");
+            }
+        }
+
+
+        static void Menu(){
+            while(true){
+                Console.WriteLine("\t\t=======MENU========");
+                Console.WriteLine("1.Thêm 1 sinh viên vào danh sach");
+                Console.WriteLine("2.Hiển thị danh sách sinh viên");
+                Console.WriteLine("3.Tìm kiếm theo địa chỉ");
+                Console.WriteLine("4.Tìm kiếm theo điểm");
+                Console.WriteLine("5.Sắp xếp theo tổng điểm: ");
+                Console.WriteLine("6.Xóa phần tử bất ký: ");
+                Console.WriteLine("7.Thoát");
+                Console.Write("Nhập vào lựa chọn: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                if(n==1){
+                 AddStudent();  
+                } 
+                else if(n==2){
+                    if(IsEmpty()){
+                        Console.WriteLine("Danh sách không có sinh viên nào: ");
+                    } else {
+                        ShowList();
+                    }
+                }
+                else if(n==3){
+                    if(IsEmpty()){
+                        Console.WriteLine("Danh sách không có sinh viên nào: ");
+                    } else {
+                        Console.Write("Nhập vào địa chỉ cần tìm kiếm: ");
+                        string DiaChi = Console.ReadLine();
+                        SearchByKey("DiaChi",DiaChi,0);
+
+                    }
+                }
+                else if(n==4){
+                    if(IsEmpty()){
+                        Console.WriteLine("Danh sách không có sinh viên nào: ");
+                    } else {
+                          Console.Write("Nhập vào tổng điểm cần tìm kiếm: ");
+                        double Tong = Convert.ToInt32(Console.ReadLine());
+                        SearchByKey("TongDiem","",Tong);
+                        
+                    }
+                }
+                else if(n==5){
+                    if(IsEmpty()){
+                        Console.WriteLine("Danh sách không có sinh viên nào: ");
+                    } else {
+                       SortByMark();
+                    }
+                }
+                else if(n==6){
+                    if(IsEmpty()){
+                        Console.WriteLine("Danh sách không có sinh viên nào: ");
+                    } else {
+                        Console.Write("Nhập vào địa chỉ muốn xóa: ");
+                        string DiaChi = Console.ReadLine();
+                        RemoveStuddent(DiaChi);
+                    }
+                } else if(n==7){
+                    Console.WriteLine("Đã thoát");
+                    break;
+                } else {
+                    Console.WriteLine("Lựa chọn bạn nhập không chính xác");
+                }
+
+
+
+            }
+        }
+
+        static void Main(string[] args){
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+          Menu();
+        }
+
+    }
+
+
+}
---
+++ SinhVienA.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiemTra3
+{
+    internal class SinhVienA
+    {
+
+        public string ID{ get; set;}
+        public string Ten { get; set;}
+
+        public string DiaChi { get; set;}
+
+        public float Toan { get; set; }
+
+        public float Ly { get; set; }
+
+        public float Hoa{ get; set; }
+
+        public float DiemUT{ get; set; }
+
+        public double Tong{
+            get
+            {
+                return Toan + Ly + Hoa + DiemUT;
+            }
+        }
+
+        public SinhVienA(){
+            this.ID = "";
+            this.Ten = "";
+            this.DiaChi = "";
+        }
+
+        public void Nhap(){
+            try{
+                Console.Write("Nhập vào ID: ");
+            this.ID = Console.ReadLine();
+
+            Console.Write("Nhập vào Họ tên: ");
+            this.Ten = Console.ReadLine();
+
+            Console.Write("Nhập vào Địa Chỉ: ");
+            this.DiaChi = Console.ReadLine();
+
+            Console.Write("Nhập vào điểm Toán ");
+            this.Toan = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Nhập vào điểm Lý: ");
+            this.Ly = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Nhập vào điểm Hóa: ");
+            this.Hoa = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Nhập vào Điểm UT: ");
+            this.DiemUT = Convert.ToSingle(Console.ReadLine());
+            } catch(Exception ){
+                Console.WriteLine("Thông tin bạn nhập không hợp lệ lên sẽ để giá trị mặc định");
+            }
+        }
+
+        public void Xuat(){
+            Console.WriteLine($"{this.ID,10} {this.Ten,15} {this.DiaChi,10} {this.Toan,10} {this.Ly,10} {this.Hoa,10} {this.DiemUT,10} {this.Tong,10}");
+        }
+
+    }
+}
